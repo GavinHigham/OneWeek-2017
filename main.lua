f51b9578f43ae397832227befa15e8456c1892e2
Gavin Higham <gavihi@microsoft.com>
2017-07-24 17:46:35 -0700

    Fixing rebase a bit

--- main.lua
+++ main.lua
@@ -2,22 +2,19 @@ require "keys"
 
 en_us = {
 	{leftgutter = 10, [[Esc]],"`","1","2","3","4","5","6","7","8","9","0","-","="},
-	{leftgutter = 15, [[Tab]],"q","w","e","r","t","y","u","i","o","p","[","]",[[\]]}, --Double square brackets are an alternative to quotes
+	{leftgutter = 15, [[Tab]],"q","w","e","r","t","y","u","i","o","p","[","]",[[\]]},
 	{leftgutter = 25, [[Caps]],"a","s","d","f","g","h","j","k","l","p","[";"]",[[']]},
 	{leftgutter = 35, [[Shift]],"z","x","c","v","b","n","m",[[,]],[[.]],[[/]],[[Shift]]},
-	{leftgutter = 45, [[Shift]],"z","x","c","v","b","n","m",[[,]],[[.]],[[/]],[[Shift]]}
 }
 
+--Used to suppress or alter printing of particular key labels.
 named_keys = {
+	Esc = "",
 	Tab = "\t",
+	Caps = "",
 	Space = " ",
 	Enter = "\n",
-}
-
-named_keys = {
-	Tab = "\t",
-	Space = " ",
-	Enter = "\n",
+	Shift = "",
 }
 
 layout = en_us
@@ -28,8 +25,6 @@ scales = {
 
 keywidth, keyheight = 50, 50 --Placeholder values, maybe we can detect DPI to change these
 keymargin_x, keymargin_y = keywidth/10, keyheight/10 --Placeholder values
-click = nil
-presses = {}
 
 function love.load()
 	love.window.setTitle("Best Keyboard Ever")
